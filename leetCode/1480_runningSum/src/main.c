ca7152ebae65b4610adcc17bc48998d576f0139a
YuriMZ-Nunes <yuri.m.z.nunes@gmail.com>
2023-03-02 16:20:06 -0300

    commit

--- leetCode/1480_runningSum/src/main.c
+++ leetCode/1480_runningSum/src/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("Hello World!\n");
+int* runningSum(int* nums, int numsSize, int* returnSize){
+    int *result = malloc(sizeof(int) * numsSize);
+    int sum = 0;
+    for(int i = 0; i < numsSize; i++){
+        sum += nums[i];
+        result[i] = sum;
+    }
+    *returnSize = sizeof(nums);
+    return result;
+}
+
+int main(){
+    int nums[4] = {1, 2, 3, 4};
+    int numsSize = 4;
+    int *returnSize = malloc(sizeof(int) * 4);
+
+    runningSum(nums, numsSize, returnSize);
 
     return 0;
 }
